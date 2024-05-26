ab4ab98a6d781562d7839815c899d387e9cde84f
Wedson Quintanilha da Silva <wquintanilhadasilva@gmail.com>
2024-05-26 16:14:22 -0300

    Adicionar query

--- src/main/java/br/com/wqs/elastic/Api.java
+++ src/main/java/br/com/wqs/elastic/Api.java
@@ -1,7 +1,13 @@
 package br.com.wqs.elastic;
 
+import co.elastic.clients.elasticsearch._types.query_dsl.BoolQuery;
+import co.elastic.clients.elasticsearch._types.query_dsl.TermQuery;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.elasticsearch.client.elc.NativeQuery;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.SearchHit;
+import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +23,7 @@ import java.util.List;
 public class Api {
 
     private final RequestRepository repository;
+    private final ElasticsearchOperations elasticsearchOperations;
 
     @GetMapping("/{id}")
     public ResponseEntity<List<RequisicaoDto>> getById(@PathVariable("id") String requestId) {
@@ -24,5 +31,41 @@ public class Api {
         var r = repository.findByRequestIdOrRequestHistory(requestId);
         var ret = r.stream().map(RequisicaoDto::create).toList();
         return ResponseEntity.ok(ret);
+    }
+
+    @GetMapping("/q/{id}")
+    public ResponseEntity<List<RequisicaoDto>> queryById(@PathVariable("id") String requestId) {
+        log.info("query [{}]", requestId);
+        BoolQuery.Builder queryBuilder = new BoolQuery.Builder();
+
+        // Adiciona a condição de OU (should) para requestId
+        queryBuilder.should(
+            new TermQuery.Builder()
+                    .field("requestId.keyword")
+                    .value(requestId)
+                    .build()
+                    ._toQuery()
+        ).should(
+            new TermQuery.Builder()
+            .field("requestHistory.keyword")
+            .value(requestId)
+            .build()
+            ._toQuery()
+        );
+
+        // Constrói a query final
+        NativeQuery query = NativeQuery.builder()
+                .withQuery(queryBuilder.build()._toQuery())
+                .build();
+
+        // Executa a busca
+        List<SearchHit<Request>> content = elasticsearchOperations
+                .search(query, Request.class, IndexCoordinates.of("requisicoes"))
+                .getSearchHits();
+
+        var ret = content.stream().map(row -> RequisicaoDto.create(row.getContent())).toList();
+
+       return ResponseEntity.ok(ret);
+
     }
 }
